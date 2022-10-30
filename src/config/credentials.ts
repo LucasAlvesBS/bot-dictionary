825b74c07d775350dfcb7e8cea750450712b6453
Lucas Alves <lucasalvesbs6@gmail.com>
2022-10-30 12:27:22 -0300

    feat: start project

---
+++ src/config/credentials.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const credentials = {
+  discordToken: process.env.DISCORD_TOKEN || '',
+  botId: process.env.BOT_ID || '',
+  guildId: process.env.GUILD_ID || '',
+};
---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { leaderboardInteraction } from '@commands/interactions/leaderboard.interaction';
+import { resetLeaderboardInteraction } from '@commands/interactions/reset-leaderboard.interaction';
+import { registerCommands } from '@commands/register-commands';
+import { credentials } from '@config/credentials';
+import { checkDiscordToken } from '@helpers/functions/discord-token.function';
+import { Client, GatewayIntentBits, Partials, REST } from 'discord.js';
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+  ],
+  partials: [Partials.Channel],
+});
+
+const rest = new REST({ version: '10' }).setToken(credentials.discordToken);
+
+checkDiscordToken();
+
+client.login(credentials.discordToken);
+
+client.on('ready', () => {
+  console.log('Discord ready to start');
+});
+
+registerCommands(rest);
